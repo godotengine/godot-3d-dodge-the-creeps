e6a17df2e022e2ab513ce4e3e339d2eee702cfa9
Nathan Lovato <nathan@gdquest.com>
2020-11-24 21:03:30 -0600

    Remove disabled collision shape on the mob
    This is unnecessary after reordering the code during prototyping

--- dodge_the_creeps_3d/Mob.gd
+++ dodge_the_creeps_3d/Mob.gd
@@ -25,8 +25,8 @@ func initialize(start_position, player_position):
 	velocity = Vector3.FORWARD * random_speed
 	# We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
 	$AnimationPlayer.playback_speed = random_speed / min_speed
-	$CollisionShape.disabled = false
 
 
 func squash():
