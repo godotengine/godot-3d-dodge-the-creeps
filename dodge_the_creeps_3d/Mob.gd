7849740e0927dd275d4f5e366e8145826b301ee4
ParadoxV5 <paradox.ver5@gmail.com>
2023-03-25 18:49:55 -0600

    Update Mob.gd

--- dodge_the_creeps_3d/Mob.gd
+++ dodge_the_creeps_3d/Mob.gd
@@ -8,12 +8,12 @@ signal squashed
 # Maximum speed of the mob in meters per second.
 @export var max_speed = 18
 
-var velocity = Vector3.ZERO
+var target_velocity = Vector3.ZERO
 
 
 func _physics_process(_delta):
 	# warning-ignore:return_value_discarded
-	set_velocity(velocity)
+	set_velocity(target_velocity)
 	move_and_slide()
 
 
@@ -23,15 +23,15 @@ func initialize(start_position, player_position):
 
 	var random_speed = randf_range(min_speed, max_speed)
 	# We calculate a forward velocity first, which represents the speed.
-	velocity = Vector3.FORWARD * random_speed
+	target_velocity = Vector3.FORWARD * random_speed
 	# We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
-	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	target_velocity = target_velocity.rotated(Vector3.UP, rotation.y)
 
 	$AnimationPlayer.playback_speed = random_speed / min_speed
 
 
 func squash():
-	emit_signal("squashed")
+	squashed.emit()
 	queue_free()
 
 
