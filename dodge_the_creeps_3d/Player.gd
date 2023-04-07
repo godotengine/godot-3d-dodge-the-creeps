a223bbd3e1b5b8e1ece3d6fa76748c037fe01cfc
ParadoxV5 <paradox.ver5@gmail.com>
2023-04-07 12:38:14 -0600

    Add `null` check to squashing
    Box 2
    Fix `null` error from previous commit

--- dodge_the_creeps_3d/Player.gd
+++ dodge_the_creeps_3d/Player.gd
@@ -55,8 +55,8 @@ func _physics_process(delta):
 	# If there are no "slides" this frame, the loop below won't run.
 	for index in range(get_slide_collision_count()):
 		var collision = get_slide_collision(index)
-		if collision.get_collider().is_in_group("mob"):
-			var mob = collision.get_collider()
+		var mob = collision.get_collider()
+		if mob != null and mob.is_in_group("mob"):
 			if Vector3.UP.dot(collision.get_normal()) > 0.1:
 				mob.squash()
 				target_velocity.y = bounce_impulse
