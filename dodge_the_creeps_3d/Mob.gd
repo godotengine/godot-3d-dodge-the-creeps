5d3f96d1d7f4114221c1dfe812f7ec7f7b65559b
ParadoxV5 <paradox.ver5@gmail.com>
2023-04-07 12:42:18 -0600

    Remove the separate `Mob#target_velocity`
    Box 4

--- dodge_the_creeps_3d/Mob.gd
+++ dodge_the_creeps_3d/Mob.gd
@@ -8,12 +8,8 @@ signal squashed
 # Maximum speed of the mob in meters per second.
 @export var max_speed = 18
 
-var target_velocity = Vector3.ZERO
-
 
 func _physics_process(_delta):
-	# warning-ignore:return_value_discarded
-	set_velocity(target_velocity)
 	move_and_slide()
 
 
@@ -23,9 +19,9 @@ func initialize(start_position, player_position):
 
 	var random_speed = randf_range(min_speed, max_speed)
 	# We calculate a forward velocity first, which represents the speed.
-	target_velocity = Vector3.FORWARD * random_speed
+	velocity = Vector3.FORWARD * random_speed
 	# We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
-	target_velocity = target_velocity.rotated(Vector3.UP, rotation.y)
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 	$AnimationPlayer.speed_scale = random_speed / min_speed
 
