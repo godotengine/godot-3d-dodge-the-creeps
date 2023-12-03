4156cc32ab9fdd0ad550fdfa70d87ceccec7a858
Kat Hogan <katkola@gmail.com>
2023-12-03 13:28:25 -0500

    updated for 4.2.stable

--- dodge_the_creeps_3d/Main.gd
+++ dodge_the_creeps_3d/Main.gd
@@ -1,35 +1,32 @@
 extends Node
 
-export(PackedScene) var mob_scene
-
+@export var mob_scene: PackedScene
 
 func _ready():
 	randomize()
 	$UserInterface/Retry.hide()
-
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
 		# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 
-
 func _on_MobTimer_timeout():
 	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instance()
+	var mob = mob_scene.instantiate()
 
 	# Choose a random location on the SpawnPath.
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-	mob_spawn_location.unit_offset = randf()
+	mob_spawn_location.progress_ratio = randf()
 
 	# Communicate the spawn location and the player's location to the mob.
 	var player_position = $Player.transform.origin
-	mob.initialize(mob_spawn_location.translation, player_position)
+	mob.initialize(mob_spawn_location.position, player_position)
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 	# We connect the mob to the score label to update the score upon squashing a mob.
-	mob.connect("squashed", $UserInterface/ScoreLabel, "_on_Mob_squashed")
+	mob.connect("squashed", Callable($UserInterface/ScoreLabel, "_on_Mob_squashed"))
 
 
 func _on_Player_hit():
